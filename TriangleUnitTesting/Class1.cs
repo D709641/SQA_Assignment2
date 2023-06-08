2a9fdd41c6c0eb496c03dbff2dc39e4a3e5be93e
Dixit Umaretiya <dixitumaretiya.12345@gmail.com>
2023-06-08 01:54:12 -0400

    ValidEquilateralTested

---
+++ TriangleUnitTesting/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+
+namespace TriangleUnitTesting
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void ValidEquilateral_Inpu15and15and15_ReturnsEquilateral()
+        {
+            // Arrange
+            int firstSide = 15;
+            int secondSide = 15;
+            int thirdSide = 15;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual("An EQUILATERAL triangle is formed", result);
+        }
+
+    }
+}
