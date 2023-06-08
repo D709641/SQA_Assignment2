9e52b4b2c59aff47536017b621e0609bc920b24d
Dixit Umaretiya <dixitumaretiya.12345@gmail.com>
2023-06-08 02:00:20 -0400

    ValidIsoscelesTested

--- TriangleUnitTesting/Class1.cs
+++ TriangleUnitTesting/Class1.cs
@@ -26,6 +26,50 @@ namespace TriangleUnitTesting
             // Assert
             Assert.AreEqual("An EQUILATERAL triangle is formed", result);
         }
+        
+        [Test]
+        public void ValidIsosceles_Input15and15and18_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 15;
+            int secondSide = 15;
+            int thirdSide = 18;
 
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual("An ISOSCELES triangle is formed", result);
+        }
+
+        [Test]
+        public void ValidIsosceles_Input5and7and7_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 7;
+            int thirdSide = 7;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual("An ISOSCELES triangle is formed", result);
+        }
+
+        [Test]
+        public void ValidIsosceles_Input6and9and9_ReturnsIsosceles()
+        {
+            // Arrange
+            int firstSide = 6;
+            int secondSide = 9;
+            int thirdSide = 9;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual("An ISOSCELES triangle is formed", result);
+        }
     }
 }
